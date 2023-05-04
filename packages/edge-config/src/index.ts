f352b2546d5930f1459f0470f4d97990273bf22a
Dominik Ferber <dominik.ferber@gmail.com>
2023-05-04 10:05:15 -0700

    add server-only to edge-config

--- packages/edge-config/src/index.ts
+++ packages/edge-config/src/index.ts
@@ -17,6 +17,7 @@ import type {
   EmbeddedEdgeConfig,
 } from './types';
 import { fetchWithCachedResponse } from './utils/fetch-with-cached-response';
+import 'server-only';
 
 export {
   parseConnectionString,
